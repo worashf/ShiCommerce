974c1e92de97eeb5c29d2a2c08193aa75bf4c7e8
worashf <uppert83@gmail.com>
2022-11-20 16:41:28 +0300

    Add route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+const products = require('./routes/product');
+app.use(express.json());
+
+app.use('/api/v1', products);
 module.exports = app;
