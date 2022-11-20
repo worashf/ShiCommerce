90b35a2d5f6984cbe1c0f9b8f6007285c708900f
worashf <uppert83@gmail.com>
2022-11-20 18:27:15 +0300

    Add database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/db');
 //set up enviroment file
 dotenv.config({ path: 'backend/config/config.env' });
+
+connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(
     `Server started at port ${process.env.PORT} in ${process.env.NODE_ENV}`
