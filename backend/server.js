6c07451c8dd3d985fca2647bdaf69809f379b239
worashf <uppert83@gmail.com>
2022-11-21 22:57:13 +0300

    Add uncaght exception handler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,13 @@ const connectDatabase = require('./config/db');
 //set up enviroment file
 dotenv.config({ path: 'backend/config/config.env' });
 
+// uncatght exception handler
+process.on('uncaughtException', (err) => {
+  console.log('Error', err.message);
+  console.log('shutting down because of uncatght excepion');
+  process.exit(1);
+});
+
 connectDatabase();
 const server = app.listen(process.env.PORT, () => {
   console.log(
