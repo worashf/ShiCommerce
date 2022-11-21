72659af8e9c76b0ec2636e7bcf4d790cdc31ddcc
worashf <uppert83@gmail.com>
2022-11-21 22:31:25 +0300

    Handle development and production error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,7 +1,24 @@
 const ErrorHandler = require('../utils/errorHandler');
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.message = err.message || 'INternal server error';
+
+  if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    res.status(err.statusCode).json({
+      status: false,
+      error: err,
+      errorMessage: err.message,
+      stack: err.stack,
+    });
+  }
+  if (process.env.NODE_ENV === 'PRODUCTION') {
+    let error = { ...err };
+    error.message = err.message;
+    res.status(err.statusCode).json({
+      status: false,
+      message: error.message || 'Internal server error',
+    });
+  }
+  err.message = err.message || 'Internal server error';
   res.status(err.statusCode).json({
     status: false,
     error: err.stack,
