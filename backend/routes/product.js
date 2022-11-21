12e46ef1c363a6657727c75bf91dfe8e808f8777
worashf <uppert83@gmail.com>
2022-11-21 20:34:53 +0300

    Add admin product routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts } = require('../controllers/productController');
+const {
+  getProducts,
+  newProduct,
+  productDetails,
+  updateProduct,
+  deleteProduct,
+} = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
+router.route('/products/:id').get(productDetails);
+router.route('/admin/products/new').post(newProduct);
+router.route('/admin/products/:id').put(updateProduct);
+router.route('/admin/products/:id').delete(deleteProduct);
 
 module.exports = router;
