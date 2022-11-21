3f2fc152b8e78692ceec05a91bc0a4975e0ed75a
worashf <uppert83@gmail.com>
2022-11-21 22:21:47 +0300

    Add error middleware

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 const products = require('./routes/product');
+const errorMidleWare = require('./middlewares/error');
 app.use(express.json());
 
+// product route
 app.use('/api/v1', products);
+
+//error handeler middleware
+app.use(errorMidleWare);
 module.exports = app;
