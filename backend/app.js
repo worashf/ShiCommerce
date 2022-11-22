6101edd027fc8cb9fc0a6d91ffdc4fe4fd259e87
worashf <uppert83@gmail.com>
2022-11-22 22:53:42 +0300

    Add user route

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 const products = require('./routes/product');
+const auth = require('./routes/auth');
 const errorMidleWare = require('./middlewares/error');
 app.use(express.json());
 
 // product route
 app.use('/api/v1', products);
-
+//user authentiction
+app.use('/api/v1', auth);
 //error handeler middleware
 app.use(errorMidleWare);
 module.exports = app;
