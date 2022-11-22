73b714f118e536b19ec2d2c52aeca478d60769ce
worashf <uppert83@gmail.com>
2022-11-22 22:54:46 +0300

    Create user registration

---
+++ backend/controllers/authController.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError = require('../middlewares/catchAsyncError');
+
+//Signup new user  /api/v1/register
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: '',
+      url: '',
+    },
+  });
+
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
