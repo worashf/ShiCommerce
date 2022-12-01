175093960777b4d11ef7e4be571bb28b627c2f38
Worash <uppert83@gmail.com>
2022-12-01 11:32:38 +0300

    Add send token method

---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,17 @@
+const sendtoken = (user, statusCode, res) => {
+    //create token
+
+    const token = user.JsonWebToken()
+    const options = {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+    res.status(statusCode).cookie("token", token, options).json({
+        success: true,
+        token,
+        user
+     })
+
+}
+
+module.exports = sendtoken
