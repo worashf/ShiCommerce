cdf2726ce36f854d142cb3935dc6b22c9fb689ac
Worash <uppert83@gmail.com>
2022-12-02 14:53:23 +0300

    Add authorize  roles method

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -18,3 +18,13 @@ exports.isAuthenticatedUser = catchAsyncError(
        next()
     }
 )
+
+// handling user roles
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req, user.role)) {
+            return next( new ErrorHandler(`Role (${req.user.role}) is not allowed to access this resource`,403)
+      )  }
+        next()
+     }
+ }
