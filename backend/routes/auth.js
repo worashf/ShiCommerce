b3889288efaf02de2da7d510908a6e215db5b1ff
Worash <uppert83@gmail.com>
2022-12-02 19:01:21 +0300

    Add password reset route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 const expess = require('express');
 const router = expess.Router();
 
-const { registerUser,loginUser, logout,forgotPassword } = require('../controllers/authController');
+const { registerUser,loginUser, logout,forgotPassword,resetPassword } = require('../controllers/authController');
 
 router.route('/signup').post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout)
 router.route("/password/forgot").post(forgotPassword)
+router.route("password/reset/:token").put(resetPassword)
 module.exports = router;
