45ece1efbeef7286be9d2e6e08a4c38f43941172
Worash <uppert83@gmail.com>
2022-12-02 14:40:29 +0300

    Create protected routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,11 +7,11 @@ const {
   updateProduct,
   deleteProduct,
 } = require('../controllers/productController');
-
+const {isAuthenticatedUser} = require("../middlewares/auth")
 router.route('/products').get(getProducts);
 router.route('/products/:id').get(productDetails);
-router.route('/admin/products/new').post(newProduct);
-router.route('/admin/products/:id').put(updateProduct);
-router.route('/admin/products/:id').delete(deleteProduct);
+router.route('/admin/products/new').post(isAuthenticatedUser, newProduct);
+router.route('/admin/products/:id').put(isAuthenticatedUser,updateProduct);
+router.route('/admin/products/:id').delete(isAuthenticatedUser, deleteProduct);
 
 module.exports = router;
