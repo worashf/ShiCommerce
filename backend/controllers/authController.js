2c91e12c7c83e5d23b7d50ef803f711d3d08f46b
Worash <uppert83@gmail.com>
2022-12-02 14:41:12 +0300

    Create logout method

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,3 +37,18 @@ catchAsyncError(async (req, res, next) => {
   }
   sendtoken(user, 200,res)
 })
+
+
+// Logout user  => /api/v1/logout
+
+exports.logout = catchAsyncError(async (req, res, next) => {
+  
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly:true
+  })
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfuly "
+ })
+})
