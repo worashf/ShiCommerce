5ee8f59b8ec3f30fb7f34e5b086665e5ea7deb0a
Worash <uppert83@gmail.com>
2022-12-02 14:39:50 +0300

    Add login and logout routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const expess = require('express');
 const router = expess.Router();
 
-const { registerUser } = require('../controllers/authController');
+const { registerUser,loginUser, logout } = require('../controllers/authController');
 
 router.route('signup').post(registerUser);
-
+router.route("login").post(loginUser);
+router.route("logout").get(logout)
 module.exports = router;
