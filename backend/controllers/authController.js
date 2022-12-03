04f01656cac9a8c2bab4eec57e4b0a8fcd1e9664
Worash <uppert83@gmail.com>
2022-12-03 10:32:56 +0300

    Add get user profile feature

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -97,6 +97,17 @@ exports.resetPassword = catchAsyncError(async (req, res, next) => {
   sendtoken(user,200, res)
 })
 
+
+// Get currently logged in user details => /api/v1/me
+exports.getUserProfile = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+  res.status(200).json({
+    success: true,
+    user
+   })
+})
+
+
 // Logout user  => /api/v1/logout
 
 exports.logout = catchAsyncError(async (req, res, next) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 const expess = require('express');
 const router = expess.Router();
 
-const { registerUser,loginUser, logout,forgotPassword,resetPassword } = require('../controllers/authController');
-
+const { registerUser, loginUser, logout, forgotPassword,
+    resetPassword, getUserProfile } = require('../controllers/authController');
+const {isAuthenticatedUser} = require("../middlewares/auth")
 router.route('/signup').post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout)
 router.route("/password/forgot").post(forgotPassword)
 router.route("password/reset/:token").put(resetPassword)
+router.route("/me").get(isAuthenticatedUser, getUserProfile)
 module.exports = router;
