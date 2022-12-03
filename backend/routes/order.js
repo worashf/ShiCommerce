8fe27b1c8c72550e72eef2b3e34713b587a8c9f1
Worash <uppert83@gmail.com>
2022-12-03 15:16:07 +0300

    Implement get sing order & logged in user oders  routes

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
-const { newOrder } = require("../controllers/orderController")
+const { newOrder,getSingleOrder,myOrders } = require("../controllers/orderController")
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth")
 
  router.route("/order/new").post(isAuthenticatedUser, newOrder)
-
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder)
+router.route("/order/me").get(isAuthenticatedUser, myOrders)
 module.exports = router
