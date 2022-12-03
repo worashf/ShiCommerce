0acb4814588c08e33d6cc6294c995c9c0a34e82c
Worash <uppert83@gmail.com>
2022-12-03 11:15:16 +0300

    Authorize get all users route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const router = expess.Router();
 const { registerUser, loginUser, logout, forgotPassword,
     resetPassword, getUserProfile, updatePassword,
     updateUserProfile, getAllUsers } = require('../controllers/authController');
-const {isAuthenticatedUser} = require("../middlewares/auth")
+const {isAuthenticatedUser, authorizeRoles} = require("../middlewares/auth")
 router.route('/signup').post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout)
@@ -14,5 +14,5 @@ router.route("/me").get(isAuthenticatedUser, getUserProfile)
 router.route("/password/update").put(isAuthenticatedUser, updatePassword)
 router.route("me/update").put(isAuthenticatedUser, updateUserProfile)
 //Admin routes
-router.route("/admin/users").get(isAuthenticatedUser,getAllUsers)
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"),getAllUsers)
 module.exports = router;
