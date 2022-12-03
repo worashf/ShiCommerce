ddf53e7206d118a473a08d77b07b8e736271a1c5
Worash <uppert83@gmail.com>
2022-12-03 15:15:56 +0300

    Implement get sing order & logged in user oders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,4 +39,28 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
     })
 
 })
- 
+
+// Get single order = > /api/v1/order/:id
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate("user",'name email')
+    if (!order) {
+        return  next(new ErrorHandler(`No oder found with id: ${req.params.id}`))
+    }
+    res.status(200).json({
+        success: true, 
+        order
+   }) 
+})
+
+
+ // Get logged in user order = > /api/v1/order/me
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find({user:req.user.id})
+    if (!orders) {
+        return  next(new ErrorHandler(`No oders found for: ${req.user.email}`))
+    }
+    res.status(200).json({
+        success: true, 
+        orders
+   }) 
+})
