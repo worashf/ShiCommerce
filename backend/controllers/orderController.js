220556913070964475f42081c738f6756bdb1b0d
Worash <uppert83@gmail.com>
2022-12-04 10:30:21 +0300

    Add admin update order process

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -80,3 +80,32 @@ exports.getAllOrders = catchAsyncError(async (req, res, next) => {
         orders,
    })
 })
+
+//Admin update process order =>  /api/v1/admin/order/:id
+
+exports.updateProcessOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+    if (order.orderStatus === "Delivered") {
+        return next(new ErrorHandler("You have already delivered this order",400))
+    }
+    order.orderItems.forEach(async orderItem => {
+        await updateStock(orderItem.product, orderItem.quantity)
+    })
+    order.orderStatus = req.body.orderStatus
+    order.deliveredAt = Date.now()
+    await order.save()
+
+
+    res.status(200).json({
+        success: true, 
+        order
+
+   })
+})
+
+
+async function updateStock(id, quantity) {
+    const product = await Product.findById(id)
+    product.stock = product.stock - quantity
+     await product.save({validateBeforeSave:false})
+}
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const { newOrder,getSingleOrder,myOrders, getAllOrders } = require("../controllers/orderController")
+const { newOrder,getSingleOrder,myOrders, getAllOrders,updateProcessOrder } = require("../controllers/orderController")
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth")
 
  router.route("/order/new").post(isAuthenticatedUser, newOrder)
@@ -9,4 +9,5 @@ router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder)
 router.route("/order/me").get(isAuthenticatedUser, myOrders)
 //admin routes
 router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders)
+router.route("/admin/order/:id").get(isAuthenticatedUser, authorizeRoles("admin"),updateProcessOrder)
 module.exports = router
