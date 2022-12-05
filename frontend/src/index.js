f1920cbf359cf48f2b63dfada59b9049c6e90837
Worash <uppert83@gmail.com>
2022-12-05 17:12:45 +0300

    Add  fontawesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'font-awesome/css/font-awesome.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
