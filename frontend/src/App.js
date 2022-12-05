836b60e2ec36afa34f4a646f9b42aed4c14d1fce
Worash <uppert83@gmail.com>
2022-12-05 17:26:33 +0300

    Add  home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
 
       <Header />
+      <Home/>
       <Footer/>
     </div>
   );
