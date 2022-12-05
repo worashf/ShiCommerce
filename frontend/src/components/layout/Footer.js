e64991cc6cf18e56eef6f81427b69c7635b64cba
Worash <uppert83@gmail.com>
2022-12-05 17:11:50 +0300

    Create footer

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const  Footer= ()=> {
+  return (
+    <footer className="py-1">
+      <p className="text-center  mt-1">
+        Worash Abocherugn - 2022, All Rights Reserved
+      </p>
+    </footer>   
+
+  )
+}
+
+export default Footer
