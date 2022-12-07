b00d38b45032428f25b96ba48b7a9827a56e1ede
Worash <uppert83@gmail.com>
2022-12-07 18:40:06 +0300

    Add  store

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,14 @@
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+
+const  rootReducer= combineReducers(
+
+)
+const store = configureStore({
+    reducer: {
+        rootReducer
+    }
+})
+
+export default store
+
