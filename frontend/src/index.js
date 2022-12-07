adde3b044261a03085f3ce9c1325f342fa5e6fe9
Worash <uppert83@gmail.com>
2022-12-07 18:41:06 +0300

    Connect to store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-
+import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    </React.StrictMode>
+    </Provider>
 );
 
 
