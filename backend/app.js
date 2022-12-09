2012f39865da22a8acfb67e748d658a6f5a10335
Worash <uppert83@gmail.com>
2022-12-09 15:36:19 +0300

    Enable cors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 const app = express();
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
 const order = require ("./routes/order")
 const errorMidleWare = require('./middlewares/error');
-
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 // const { request } = require('express');
 app.use(express.json());
 app.use(cookieParser())
