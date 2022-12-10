63b2ac9d973b1f9b9deca60e1cb1021ddca33b8e
Worash <uppert83@gmail.com>
2022-12-10 18:04:15 +0300

    Create product detail action

--- frontend/src/redux/actions/productAction.js
+++ frontend/src/redux/actions/productAction.js
@@ -1,4 +1,7 @@
-import { ALL_PRODUCTS_FAIL, ALL_PRODUCTS_SUCCESS, ALL_PRODUCTS_REQUEST, CLEAR_ERRORS } from "../constants/productConstants";
+import {
+    ALL_PRODUCTS_FAIL, ALL_PRODUCTS_SUCCESS, ALL_PRODUCTS_REQUEST, CLEAR_ERRORS,
+    GET_PRODUCT_DETAILS_REQUEST, GET_PRODUCT_DETAILS_SUCCESS, GET_PRODUCT_DETAILS_FAIL
+} from "../constants/productConstants";
 import axios  from "axios"
 export const getAllProducts = () => async(dispatch) => {
     try {
@@ -20,6 +23,23 @@ export const getAllProducts = () => async(dispatch) => {
             payload:error.response.data.errorMessage
         })
  }
+}
+
+export const getProductdetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: GET_PRODUCT_DETAILS_REQUEST })
+        const { data } = await axios.get(`/api/v1/products/${id}`)
+        dispatch({
+            type: GET_PRODUCT_DETAILS_SUCCESS,
+            payload:data.product
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: GET_PRODUCT_DETAILS_FAIL, 
+            payload:error.response.data.errorMessage
+        })
+    }
 }
 
 export const clearErrors = () => async (dispatch) => {
