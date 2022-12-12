5fd228124ac6432e3225e01d4fdfbb5d9c236f7f
Worash <uppert83@gmail.com>
2022-12-12 09:34:52 +0300

    Create search component

--- frontend/src/components/layout/Search.js
+++ frontend/src/components/layout/Search.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom"
 
 const Search = () => {
+
+  const [keyword, setKeyword] = useState("")
+  const navigate = useNavigate()
+  const searchHandler = (e) => {
+    e.preventDefault()
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`)
+    }
+    else {
+      navigate("/")
+    }
+  }
   return (
-    <div>Search</div>
+    <form onSubmit={searchHandler}>
+      <div className="input-group">
+          <input
+            type="text"
+            id="search_field"
+            className="form-control"
+          placeholder="Enter Product Name ..."
+          onChange={(e)=>setKeyword(e.target.value)}
+          />
+          <div className="input-group-append">
+            <button id="search_btn" className="">
+              <i className="fa fa-search" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+    </form>
   )
 }
 
