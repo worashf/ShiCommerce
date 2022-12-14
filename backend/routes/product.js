7605a282fb2451b072c7477da48fea0ad26aa27d
Worash <uppert83@gmail.com>
2022-12-14 09:05:01 +0300

    Add create, get and delete product review routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,6 +6,9 @@ const {
   productDetails,
   updateProduct,
   deleteProduct,
+  createProductReview,
+  getProductReviews,
+  deleteProductReview
 } = require('../controllers/productController');
 const {isAuthenticatedUser} = require("../middlewares/auth")
 router.route('/products').get(getProducts);
@@ -13,5 +16,8 @@ router.route('/products/:id').get(productDetails);
 router.route('/admin/products/new').post(isAuthenticatedUser, newProduct);
 router.route('/admin/products/:id').put(isAuthenticatedUser,updateProduct);
 router.route('/admin/products/:id').delete(isAuthenticatedUser, deleteProduct);
+router.route("/review").put(isAuthenticatedUser, createProductReview)
+router.route("/reviews").get(isAuthenticatedUser, getProductReviews)
+router.route("/reviews").delete(isAuthenticatedUser, deleteProductReview)
 
 module.exports = router;
