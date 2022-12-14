b255eac02b96ce512e3773d719e8234568c036ae
Worash <uppert83@gmail.com>
2022-12-14 09:29:55 +0300

    Add filter by ratings

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -28,18 +28,19 @@ const Home = () => {
    const[price, setPrice] = useState(1000)
   const [currentPage, setCurrentPage] = useState(1)
   const [category, setCategory] = useState("")
+  const[rating,setRating] = useState(0)
   
   const {keyword} = useParams()
   const dispatch = useDispatch()
   const { error, productsCount, loading, products ,resPerPage,   filteredProductsCount} = useSelector(state => state.products)
 
   useEffect(() => {
-    dispatch(getAllProducts(keyword,currentPage,price,category))
+    dispatch(getAllProducts(keyword,currentPage,price,category, rating))
 
     if (error) {
    return toast.error(error);
     }
-  }, [dispatch,error,keyword, currentPage, price,category])
+  }, [dispatch,error,keyword, currentPage, price,category, rating])
   
 
   const setCurrentPageNum = (pageNum) => {
@@ -94,6 +95,31 @@ const Home = () => {
                         </ul>
 
                       </div>
+
+                      <hr className='my-3' />
+
+<div className='mt-'>
+  <h3> Ratings</h3>
+  <ul>
+    {
+ [5,4,3,2,1].map((rating ,index)=> (
+        <li key={index} style={{
+          "cursor": "pointer",
+          "color":"#0965d6",
+              "listStyle":"none"
+   }} onClick={() => setRating(rating)}>
+     <div className='rating-outer'>
+       <div className='rating-inner' style={{width:`${rating*20}%`}}>
+         
+       </div>
+       
+     </div>
+            </li>
+      ))
+    }
+  </ul>
+
+</div>
                     </div>
               </div>
               <div className='col-6 col-md-9'>
