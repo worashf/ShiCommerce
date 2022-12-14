d37dc4f79bb63ed2bad1626e9233d041a4afdcfd
Worash <uppert83@gmail.com>
2022-12-14 09:04:33 +0300

    Add user ref to product reviews

--- backend/models/product.js
+++ backend/models/product.js
@@ -69,7 +69,14 @@ const productSchema = mongoose.Schema({
     default: 0,
   },
   reviews: [
+  
+
     {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required:true
+    },
       name: {
         type: String,
         required: true,
