a3187f563966983a87e64ad592af7fc945ed84e1
Worash <uppert83@gmail.com>
2022-12-16 18:09:42 +0300

    Add user registration action

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -1,8 +1,11 @@
  import axios from "axios"
-import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL, CLEAR_ERRORS } from "../constants/userConstants"
+import {
+    LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL,
+    REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL, CLEAR_ERRORS
+} from "../constants/userConstants"
  
 
-export const login = (email, passsword) => async (dispatch) => {
+export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({ type: LOGIN_REQUEST })
         const config = {
@@ -11,7 +14,7 @@ export const login = (email, passsword) => async (dispatch) => {
                 
             }
         }
-        const { data } = await axios.post("/api/v1/login", { email, passsword }, config)
+        const { data } = await axios.post("/api/v1/login", { email, password }, config)
         dispatch({
             type: LOGIN_SUCCESS,
             payload: data.user
@@ -20,7 +23,39 @@ export const login = (email, passsword) => async (dispatch) => {
     catch (err) {
         dispatch({
             type: LOGIN_FAIL,
-          payload:error.response.data.errorMessage
+          payload:err.response.data.errorMessage
         })
  }
+}
+
+export const registerUser = (userData) => async (dispatch) => {
+
+    try {
+    
+        dispatch({ type: REGISTER_USER_REQUEST })
+     
+        const config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+
+        const { data } = await axios.post('/api/v1/signup', userData, config)
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+
+    }
+    catch (err) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
+        payload: err.response.data.errorMessage})
+    }
+}
+export const clearErrors = () => async (dispatch) => {
+    dispatch({
+        type: CLEAR_ERRORS,
+
+    })
 }
