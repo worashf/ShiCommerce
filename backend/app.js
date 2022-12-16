d5a5904f1f5df860fba77816f8769edf3bbf689f
Worash <uppert83@gmail.com>
2022-12-16 18:13:24 +0300

    Add   express urlencoded data parser

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
+const bodyParser = require('body-parser')
 const cors = require("cors")
+
 const app = express();
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
 const order = require ("./routes/order")
 const errorMidleWare = require('./middlewares/error');
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
-// const { request } = require('express');
+
+
+
+
+app.use(cors())
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+
 // product route
 app.use('/api/v1', products);
 //user authentiction
