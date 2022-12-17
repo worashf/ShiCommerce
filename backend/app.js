e3f2d5e02cc9546047cd32a7127bf2cf56258f4c
Worash <uppert83@gmail.com>
2022-12-17 13:24:15 +0300

    Add multer to parse multipart form data

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
 const bodyParser = require('body-parser')
+const multer = require("multer");
+const upload = multer();
 const cors = require("cors")
 
 const app = express();
@@ -13,11 +15,13 @@ const errorMidleWare = require('./middlewares/error');
 
 
 
-app.use(cors())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use(cookieParser())
-
+// for parsing multipart/form-data
+app.use(upload.array());
 // product route
 app.use('/api/v1', products);
 //user authentiction
