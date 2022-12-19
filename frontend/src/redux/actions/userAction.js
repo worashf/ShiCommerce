a8e8e9b91bfc459b9efff0682b99b901939a6fdb
Worash <uppert83@gmail.com>
2022-12-19 17:42:54 +0300

    Implement load user action

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -1,7 +1,9 @@
  import axios from "axios"
 import {
     LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL,
-    REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL, CLEAR_ERRORS
+    REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
+    LOAD_USER_REQUEST,LOAD_USER_SUCCESS,LOAD_USER_FAIL, CLEAR_ERRORS
 } from "../constants/userConstants"
  
 
@@ -55,6 +57,28 @@ export const registerUser = (userData) => async (dispatch) => {
         payload: err.response.data.errorMessage})
     }
 }
+
+
+// load  user
+
+export const loadUser = () => async (dispatch) => {
+    try {
+        dispatch({ type: LOAD_USER_REQUEST })
+        
+        const { data } = await axios.get("/api/v1/me")
+         
+        dispatch({
+            type: LOAD_USER_SUCCESS,
+            payload : data.user
+        })
+    }
+    catch (err) {
+        dispatch({
+            type: LOAD_USER_FAIL,
+        payload: err.response.data.errorMessage})
+    }
+}
+
 export const clearErrors = () => async (dispatch) => {
     dispatch({
         type: CLEAR_ERRORS,
