2fbca3eb8d031d769d1d3110fee070f8d2fc791b
Worash <uppert83@gmail.com>
2022-12-19 17:54:49 +0300

    Show  user in nav

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,37 +1,85 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Search from './Search'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Dropdown } from "react-bootstrap";
+import Search from "./Search";
 const Header = () => {
-
+  const { loading, user } = useSelector((state) => state.auth);
   return (
-      <>
-     <nav className="navbar row">
-      <div className="col-12 col-md-3">
+    <>
+      <nav className="navbar row">
+        <div className="col-12 col-md-3">
           <div className="navbar-brand">
             <Link to="/">
-            <img src="/images/logo.png" />
+              <img src="/images/logo.png" />
             </Link>
-
+          </div>
         </div>
-      </div>
 
         <div className="col-12 col-md-6 mt-2 mt-md-0">
-     
-        <Search />
-      
+          <Search />
+        </div>
 
-      </div>
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center row">
+          <div className="col-4  mt-2">
+            <Link to="/cart" style={{ textDecoration: "none" }}>
+              <span id="cart" className="mx-2">
+                Cart
+              </span>
+              <span className="ml-3" id="cart_count">
+                2
+              </span>
+            </Link>
+          </div>
 
-      <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-        <Link to ="/login" className="btn mx-2" id="login_btn">Login</Link>
+          {user ? (
+            <div className="col-6 d-flex  ">
+              <div className="ml-4">
+                <figure className="avatar nav-avatar">
+                  <img
+                    src={user.avatar && user.avatar.url}
+                    alt={user.name}
+                    className="rounded-circle"
+                  />
+                </figure>
+              </div>
+              <Dropdown className="px-4">
+                <Dropdown.Toggle split id="dropdown-split-basic">
+                  {user && user.name}
+                </Dropdown.Toggle>
 
-        <span id="cart" className="mx-2">Cart</span>
-        <span className="ml-3" id="cart_count">2</span>
-      </div>
-    </nav>
+                <Dropdown.Menu>
+                  {user && user.role !== "admin" ? (
+                    <Link to="/orders/me" style={{ textDecoration: "none" }}>
+                      <Dropdown.Item>Orders</Dropdown.Item>
+                    </Link>
+                  ) : (
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                      <Dropdown.Item>Dashboard</Dropdown.Item>
+                    </Link>
+                  )}
 
-      </>
-  )
-}
+                  <Link to="/me" style={{ textDecoration: "none" }}>
+                    <Dropdown.Item>Profile</Dropdown.Item>
+                  </Link>
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    <Dropdown.Item>Logout</Dropdown.Item>
+                  </Link>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          ) : (
+              !loading && (
+                
+              <Link to="/login" className="btn mx-2 col-3" id="login_btn">
+                Login
+              </Link>
+            )
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Header
+export default Header;
