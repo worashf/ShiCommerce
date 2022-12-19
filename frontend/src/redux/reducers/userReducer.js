d78372950a9dc9e29249d034a4cadf59b691c545
Worash <uppert83@gmail.com>
2022-12-19 17:56:19 +0300

    Add  user load

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -2,7 +2,7 @@
 import {
     LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL,
     REGISTER_USER_REQUEST,REGISTER_USER_SUCCESS,REGISTER_USER_FAIL
-
+    ,LOAD_USER_REQUEST,LOAD_USER_SUCCESS,LOAD_USER_FAIL
     , CLEAR_ERRORS
 } from '../constants/userConstants'
 
@@ -10,20 +10,30 @@ export const authReducer = (state= {user:{}}, {type, payload}) => {
     switch (type) {
         case LOGIN_REQUEST:
         case REGISTER_USER_REQUEST:
+        case  LOAD_USER_REQUEST:
          return {
              loading: true,
              isAuthenticated:false
             }
         case LOGIN_SUCCESS:
         case REGISTER_USER_SUCCESS:
+        case LOAD_USER_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 isAuthenticated: true,
                 user: payload
             }
+        case LOAD_USER_FAIL:
+            return {
+                loading: false,
+                isAuthenticated: false,
+                user: null, 
+                error : payload
+            }
         case LOGIN_FAIL:
         case REGISTER_USER_FAIL:
+
             return {
                 ...state,
                 loading: false,
