ad8fe496c55db65534511ffdd1057513d824f5f5
Worash <uppert83@gmail.com>
2022-12-21 12:28:00 +0300

    Add change password and forget password

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -1,9 +1,12 @@
  import axios from "axios"
+
 import {
     LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL,
     REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
-    LOAD_USER_REQUEST,LOAD_USER_SUCCESS,LOAD_USER_FAIL,LOGOUT_SUCCESS,LOGOUT_FAIL, CLEAR_ERRORS
+    LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAIL, LOGOUT_SUCCESS, LOGOUT_FAIL,
+    UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL,
+    UPDATE_PASSWORD_REQUEST,UPDATE_PASSWORD_SUCCESS,UPDATE_PASSWORD_FAIL,FORGOT_PASSWORD_REQUEST,FORGOT_PASSWORD_SUCCESS,FORGOT_PASSWORD_FAIL,CLEAR_ERRORS
 } from "../constants/userConstants"
  
 
@@ -59,6 +62,91 @@ export const registerUser = (userData) => async (dispatch) => {
 }
 
 
+
+export const updateProfile = (userData) => async (dispatch) => {
+
+    try {
+
+        dispatch({ type: UPDATE_PROFILE_REQUEST })
+     
+        const config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+
+
+
+        const { data } = await axios.put('/api/v1/me/update', userData,config)
+        dispatch({
+            type: UPDATE_PROFILE_SUCCESS,
+            payload: data.success
+        })
+
+    }
+    catch (err) {
+        dispatch({
+            type: UPDATE_PROFILE_FAIL,
+        payload: err.response.data.errorMessage})
+    }
+}
+
+
+// Forgot password
+export const forgetPassword = (email) => async (dispatch) => {
+    try {
+
+        dispatch({ type: FORGOT_PASSWORD_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post('/api/v1/password/forgot', email, config)
+
+        dispatch({
+            type: FORGOT_PASSWORD_SUCCESS,
+            payload: data.message
+        })
+
+    } catch (error) {
+        dispatch({
+            type: FORGOT_PASSWORD_FAIL,
+            payload: error.response.data.errorMessage
+        })
+    }
+}
+
+export const updatePassword = (passwords) => async (dispatch) => {
+
+    try {
+
+        dispatch({ type: UPDATE_PASSWORD_REQUEST })
+     
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+
+
+        const { data } = await axios.put('/api/v1/password/update', passwords,config)
+        dispatch({
+            type: UPDATE_PASSWORD_SUCCESS,
+            payload: data.success
+        })
+
+    }
+    catch (err) {
+        dispatch({
+            type: UPDATE_PASSWORD_FAIL,
+        payload: err.response.data.errorMessage})
+    }
+}
+
 // load  user
 
 export const loadUser = () => async (dispatch) => {
@@ -78,6 +166,8 @@ export const loadUser = () => async (dispatch) => {
         payload: err.response.data.errorMessage})
     }
 }
+
+
 
 export const logout = () => async (dispatch) => {
     try {
@@ -99,3 +189,4 @@ export const clearErrors = () => async (dispatch) => {
 
     })
 }
+
