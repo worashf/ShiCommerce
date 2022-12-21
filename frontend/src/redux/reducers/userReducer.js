961d994df3af62a3613225c5d4abde797b32189a
Worash <uppert83@gmail.com>
2022-12-21 14:50:44 +0300

    Add  new password

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -5,7 +5,8 @@ import {
     , LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAIL, LOGOUT_SUCCESS, LOGOUT_FAIL,
     UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL,
     UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAIL, UPDATE_PROFILE_RESET,
-    UPDATE_PASSWORD_RESET,FORGOT_PASSWORD_REQUEST,FORGOT_PASSWORD_SUCCESS,FORGOT_PASSWORD_FAIL,
+    UPDATE_PASSWORD_RESET, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, FORGOT_PASSWORD_FAIL,
+    NEW_PASSWORD_REQUEST,NEW_PASSWORD_SUCCESS,NEW_PASSWORD_FAIL,
      CLEAR_ERRORS
 } from '../constants/userConstants'
 
@@ -106,6 +107,7 @@ export const forgotPassword = (state = {}, { type, payload }) => {
     switch (type) {
        
         case FORGOT_PASSWORD_REQUEST:
+        case NEW_PASSWORD_REQUEST:
             return {
                 ...state,
                 loading: true,
@@ -117,7 +119,14 @@ export const forgotPassword = (state = {}, { type, payload }) => {
                 loading: false,
                 massage:payload
             }
-            case FORGOT_PASSWORD_FAIL:
+        case NEW_PASSWORD_SUCCESS:
+            return {
+                ...state,
+                success: payload
+                
+            }
+        case FORGOT_PASSWORD_FAIL:
+        case NEW_PASSWORD_FAIL:
                     return {
                         ...state,
                         loading: false,
