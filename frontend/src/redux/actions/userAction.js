b69edc30ad52212ddf7658d36a04b236468c6000
Worash <uppert83@gmail.com>
2022-12-21 14:59:29 +0300

    Add reset password

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -6,7 +6,10 @@ import {
     REGISTER_USER_FAIL,
     LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAIL, LOGOUT_SUCCESS, LOGOUT_FAIL,
     UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL,
-    UPDATE_PASSWORD_REQUEST,UPDATE_PASSWORD_SUCCESS,UPDATE_PASSWORD_FAIL,FORGOT_PASSWORD_REQUEST,FORGOT_PASSWORD_SUCCESS,FORGOT_PASSWORD_FAIL,CLEAR_ERRORS
+    UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAIL, FORGOT_PASSWORD_REQUEST,
+    FORGOT_PASSWORD_SUCCESS, FORGOT_PASSWORD_FAIL,
+    NEW_PASSWORD_REQUEST,NEW_PASSWORD_SUCCESS,NEW_PASSWORD_FAIL,
+    CLEAR_ERRORS
 } from "../constants/userConstants"
  
 
@@ -143,6 +146,35 @@ export const updatePassword = (passwords) => async (dispatch) => {
     catch (err) {
         dispatch({
             type: UPDATE_PASSWORD_FAIL,
+        payload: err.response.data.errorMessage})
+    }
+}
+
+// reset password 
+export const resetPassword = (token, passwords) => async (dispatch) => {
+
+    try {
+
+        dispatch({ type: NEW_PASSWORD_REQUEST })
+     
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+
+
+        const { data } = await axios.put(`/api/v1/password/reset/${token}`, passwords,config)
+        dispatch({
+            type: NEW_PASSWORD_SUCCESS,
+            payload: data.success
+        })
+
+    }
+    catch (err) {
+        dispatch({
+            type: NEW_PASSWORD_FAIL,
         payload: err.response.data.errorMessage})
     }
 }
