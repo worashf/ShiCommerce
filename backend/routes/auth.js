027cf1890de208fe97831e6c47ee4d54fea8a78b
Worash <uppert83@gmail.com>
2022-12-21 14:20:17 +0300

    Fix typo

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,10 +9,10 @@ router.route('/signup').post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout)
 router.route("/password/forgot").post(forgotPassword)
-router.route("password/reset/:token").put(resetPassword)
+router.route("/password/reset/:token").put(resetPassword)
 router.route("/me").get(isAuthenticatedUser, getUserProfile)
 router.route("/password/update").put(isAuthenticatedUser, updatePassword)
-router.route("me/update").put(isAuthenticatedUser, updateUserProfile)
+router.route("/me/update").put(isAuthenticatedUser, updateUserProfile)
 //Admin routes
 
 router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers)
