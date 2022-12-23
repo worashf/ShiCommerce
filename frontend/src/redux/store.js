0f4ac02cd9f9926d58952691abbe69ff0e28af71
Worash <uppert83@gmail.com>
2022-12-23 12:19:52 +0300

    Add cart store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,17 +1,26 @@
 
 import { configureStore} from "@reduxjs/toolkit"
 import { productReducer, productDetailReducer } from "./reducers/productReducer"
-import { authReducer,userReducer, forgotPassword} from "./reducers/userReducer"
+import { authReducer, userReducer, forgotPassword } from "./reducers/userReducer"
+import { cartReducer } from "./reducers/cartReducer"
+
+const preLoadedCartState = {
+  cart: {
+     cartItems: localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")): []
+   }
+ }
 
 const rootReducer = {
   products: productReducer,
   product: productDetailReducer,
   auth: authReducer,
   user: userReducer,
-  forgotPassword: forgotPassword
+  forgotPassword: forgotPassword,
+  cart:cartReducer
 }
 const store = configureStore({
-  reducer:  rootReducer
+  reducer: rootReducer,
+  preLoadedCartState
 })
 
 export default store
