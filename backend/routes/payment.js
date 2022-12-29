8842e59719f7f2ed7c346647d20c13b37054ca9f
Worash <uppert83@gmail.com>
2022-12-29 18:16:35 +0300

    Add payment route

---
+++ backend/routes/payment.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {processPayment,sendStripeApi}  = require("../controllers/paymentController")
+
+const { isAuthenticatedUser} = require("../middlewares/auth")
+
+router.route("/process/payment").post(isAuthenticatedUser, processPayment)
+
+router.route("/stripe-api-key").get( isAuthenticatedUser, sendStripeApi)
+
+
+module.exports = router
