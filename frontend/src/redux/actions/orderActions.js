95e9e4591e81bd9177d18bdcf28c43cc919b3b05
Worash <uppert83@gmail.com>
2023-01-02 18:44:25 +0300

    Implement get order details

--- frontend/src/redux/actions/orderActions.js
+++ frontend/src/redux/actions/orderActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {
     CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, CREATE_ORDER_FAIL,
-    MY_ORDER_REQUEST, MY_ORDER_SUCCESS, MY_ORDER_FAIL, CLEAR_ERRORS
+    MY_ORDER_REQUEST, MY_ORDER_SUCCESS, MY_ORDER_FAIL, ORDER_DETAILS_REQUEST,ORDER_DETAILS_SUCCESS,ORDER_DETAILS_FAIL, CLEAR_ERRORS
 } from "../constants/orderConstants"
 
 
@@ -50,6 +50,27 @@ export const getMyOrders = () => async (dispatch) => {
     catch (error) {
         dispatch({
             type: MY_ORDER_FAIL,
+            payload: error.response.data.errorMessage
+        })
+    }
+}
+
+// get order details 
+export const orderDetails = (id) => async (dispatch) => {
+    
+    try {
+        
+        dispatch({ type: ORDER_DETAILS_REQUEST })
+        const { data } = await axios.get(`/api/v1/order/${id}`)
+  
+        dispatch({
+            type: ORDER_DETAILS_SUCCESS,
+            payload: data.order
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: ORDER_DETAILS_FAIL,
             payload: error.response.data.errorMessage
         })
     }
