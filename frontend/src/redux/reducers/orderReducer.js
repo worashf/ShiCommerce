de65ce68da89bb23d5231940455e27fe51443cca
Worash <uppert83@gmail.com>
2023-01-02 18:39:49 +0300

    Add order detail reducer

--- frontend/src/redux/reducers/orderReducer.js
+++ frontend/src/redux/reducers/orderReducer.js
@@ -1,6 +1,6 @@
 import {
     CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, CREATE_ORDER_FAIL,
-    MY_ORDER_REQUEST,MY_ORDER_SUCCESS,MY_ORDER_FAIL,
+    MY_ORDER_REQUEST,MY_ORDER_SUCCESS,MY_ORDER_FAIL, ORDER_DETAILS_REQUEST,ORDER_DETAILS_SUCCESS,ORDER_DETAILS_FAIL,
     CLEAR_ERRORS
 } from "../constants/orderConstants"
 
@@ -46,6 +46,34 @@ export const myOrderReducer = (state={orders:[]},{type,payload}) => {
                 orders :payload
             }
         case MY_ORDER_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state, 
+                error:null
+            }
+        default:
+            return state
+    }
+}
+
+
+export const orderDetailsReducer = (state={order:{}},{type,payload}) => {
+    
+    switch (type) {
+        case ORDER_DETAILS_REQUEST:
+            return {
+                loading: true
+            }
+        case ORDER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                order :payload
+            }
+        case ORDER_DETAILS_FAIL:
             return {
                 loading: false,
                 error: payload
