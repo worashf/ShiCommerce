eca624dcc117f3f2a4df0a7bfd212bea5888c1a4
Worash <uppert83@gmail.com>
2023-01-05 18:27:50 +0300

    Create admin dashboard

---
+++ frontend/src/components/admin/Dashboard.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import MetaData from "../layout/MetaData";
+import Loader from "../layout/Loader";
+import Sidebar from "./Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div className="row">
+      <div className="col-12 col-md-2">
+        <Sidebar />
+      </div>
+      <div className="col-12 col-md-10 main">
+        <h1 className="my-4">Dashboard</h1>
+
+        <>
+          <MetaData title={"Admin Dashboard"} />
+
+          <div className="row pr-4">
+            <div className="col-xl-12 col-sm-12 mb-3">
+              <div className="card text-white bg-primary o-hidden h-100">
+                <div className="card-body">
+                  <div className="text-center card-font-size">
+                    Total Amount
+                    <br /> <b>Amount</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row pr-4">
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-success o-hidden h-100">
+                <div className="card-body">
+                  <div className="text-center card-font-size">
+                    Products
+                    <br /> <b> Products</b>
+                  </div>
+                </div>
+                <Link
+                  className="card-footer text-white clearfix small z-1"
+                  to="/admin/products"
+                >
+                  <span className="float-left">View Details</span>
+                  <span className="float-right">
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </Link>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-danger o-hidden h-100">
+                <div className="card-body">
+                  <div className="text-center card-font-size">
+                    Orders
+                    <br /> <b>Orders</b>
+                  </div>
+                </div>
+                <Link
+                  className="card-footer text-white clearfix small z-1"
+                  to="/admin/orders"
+                >
+                  <span className="float-left">View Details</span>
+                  <span className="float-right">
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </Link>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-info o-hidden h-100">
+                <div className="card-body">
+                  <div className="text-center card-font-size">
+                    Users
+                    <br /> <b>Users</b>
+                  </div>
+                </div>
+                <Link
+                  className="card-footer text-white clearfix small z-1"
+                  to="/admin/users"
+                >
+                  <span className="float-left">View Details</span>
+                  <span className="float-right">
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </Link>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-warning o-hidden h-100">
+                <div className="card-body">
+                  <div className="text-center card-font-size">
+                    Out of Stock
+                    <br /> <b>Best done</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
