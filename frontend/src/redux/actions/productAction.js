41531aeb453bbe8b95cb45b6928be1466451c668
Worash <uppert83@gmail.com>
2023-01-11 18:37:57 +0300

    Implement get all admin product and new product

--- frontend/src/redux/actions/productAction.js
+++ frontend/src/redux/actions/productAction.js
@@ -1,7 +1,9 @@
 import {
     ALL_PRODUCTS_FAIL, ALL_PRODUCTS_SUCCESS, ALL_PRODUCTS_REQUEST, CLEAR_ERRORS,
     GET_PRODUCT_DETAILS_REQUEST, GET_PRODUCT_DETAILS_SUCCESS, GET_PRODUCT_DETAILS_FAIL,
-    NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAIL
+    NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAIL,
+    NEW_PRODUCT_REQUEST,NEW_PRODUCT_SUCCESS,NEW_PRODUCT_FAIL,ADMIN_PRODUCTS_REQUEST,
+    ADMIN_PRODUCTS_SUCCESS, ADMIN_PRODUCTS_FAIL, DELETE_PRODUCT_REQUEST, DELETE_PRODUCT_SUCCESS,DELETE_PRODUCT_FAIL
 } from "../constants/productConstants";
 import axios  from "axios"
 export const getAllProducts = (keyword="", currentPage =1,price,category,rating=0) => async(dispatch) => {
@@ -71,7 +73,71 @@ export const  createReview = (review) => async (dispatch) => {
     }
 }
 
+export const newProduct = (productData) => async (dispatch) => {
+    try {
 
+        dispatch({ type: NEW_PRODUCT_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post(`/api/v1/admin/products/new`, productData, config)
+
+        dispatch({
+            type: NEW_PRODUCT_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: NEW_PRODUCT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+
+export const  getAllAdminProducts = () => async (dispatch) => {
+    try {
+        dispatch({ type:ADMIN_PRODUCTS_REQUEST})
+        const { data } = await axios.get("/api/v1/admin/products")
+        dispatch({
+            type: ADMIN_PRODUCTS_SUCCESS,
+            payload:data.products
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: ADMIN_PRODUCTS_FAIL, 
+            payload:error.response.data.errorMessage
+        })
+    }
+}
+
+
+// Delete product (Admin)
+export const deleteProduct = (id) => async (dispatch) => {
+    try {
+
+        dispatch({ type: DELETE_PRODUCT_REQUEST })
+
+        const { data } = await axios.delete(`/api/v1/admin/product/${id}`)
+
+        dispatch({
+            type: DELETE_PRODUCT_SUCCESS,
+            payload: data.success
+        })
+
+    } catch (error) {
+        dispatch({
+            type: DELETE_PRODUCT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
 export const clearErrors = () => async (dispatch) => {
     dispatch({
         type: CLEAR_ERRORS,
