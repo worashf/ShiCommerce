4586c8e405fc2baa877ce0ee8d2a006ff5e158f5
Worash <uppert83@gmail.com>
2023-01-11 18:36:21 +0300

    Add all users reducer

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -6,7 +6,8 @@ import {
     UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL,
     UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAIL, UPDATE_PROFILE_RESET,
     UPDATE_PASSWORD_RESET, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, FORGOT_PASSWORD_FAIL,
-    NEW_PASSWORD_REQUEST,NEW_PASSWORD_SUCCESS,NEW_PASSWORD_FAIL,
+    NEW_PASSWORD_REQUEST, NEW_PASSWORD_SUCCESS, NEW_PASSWORD_FAIL,
+    ALL_USERS_REQUEST,ALL_USERS_SUCCESS,ALL_USERS_FAIL,
      CLEAR_ERRORS
 } from '../constants/userConstants'
 
@@ -141,4 +142,39 @@ export const forgotPassword = (state = {}, { type, payload }) => {
         default:
             return state
    }
+}
+
+
+export const allUsersReducer = (state = { users: [] }, action) => {
+    switch (action.type) {
+
+        case ALL_USERS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+
+        case ALL_USERS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                users: action.payload
+            }
+
+        case ALL_USERS_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state;
+    }
 }
