a057d675003e858fe08db6380012f1b40c313338
Worash <uppert83@gmail.com>
2023-01-11 18:36:39 +0300

    Add all orders reducer

--- frontend/src/redux/reducers/orderReducer.js
+++ frontend/src/redux/reducers/orderReducer.js
@@ -1,6 +1,7 @@
 import {
     CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, CREATE_ORDER_FAIL,
-    MY_ORDER_REQUEST,MY_ORDER_SUCCESS,MY_ORDER_FAIL, ORDER_DETAILS_REQUEST,ORDER_DETAILS_SUCCESS,ORDER_DETAILS_FAIL,
+    MY_ORDER_REQUEST, MY_ORDER_SUCCESS, MY_ORDER_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL,
+    ALL_ORDERS_REQUEST, ALL_ORDERS_SUCCESS,ALL_ORDERS_FAIL,
     CLEAR_ERRORS
 } from "../constants/orderConstants"
 
@@ -85,5 +86,36 @@ export const orderDetailsReducer = (state={order:{}},{type,payload}) => {
             }
         default:
             return state
+    }
+}
+
+export const allOrdersReducer = (state = { orders: [] }, action) => {
+    switch (action.type) {
+
+        case ALL_ORDERS_REQUEST:
+            return {
+                loading: true
+            }
+
+        case ALL_ORDERS_SUCCESS:
+            return {
+                loading: false,
+                orders: action.payload.orders,
+                totalAmount: action.payload.totalAmount
+            }
+
+        case ALL_ORDERS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state;
     }
 }
