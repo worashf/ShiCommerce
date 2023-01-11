0afe0d7f0f0ea1cfa980e185fd0f15bff8751b20
Worash <uppert83@gmail.com>
2023-01-11 18:34:13 +0300

    Display  information

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import MetaData from "../layout/MetaData";
 import Loader from "../layout/Loader";
 import Sidebar from "./Sidebar";
+import { allUsers } from "../../redux/actions/userAction";
+import { allOrders } from "../../redux/actions/orderActions";
+import {getAllAdminProducts} from "../../redux/actions/productAction"
+
+
+
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  const { products } = useSelector(state => state.products)
+  const { users } = useSelector(state => state.allUsers)
+  const { orders, totalAmount, loading } = useSelector(state => state.allOrders)
+
+
+  let outOfStock = 0;
+  products.forEach(product => {
+      if (product.stock === 0) {
+          outOfStock += 1;
+      }
+  })
+  useEffect(() => {
+    dispatch(getAllAdminProducts())
+    dispatch(allOrders())
+    dispatch(allUsers())
+}, [dispatch])
+
   return (
     <div className="row">
       <div className="col-12 col-md-2">
@@ -13,96 +38,99 @@ const Dashboard = () => {
       </div>
       <div className="col-12 col-md-10 main">
         <h1 className="my-4">Dashboard</h1>
-
-        <>
-          <MetaData title={"Admin Dashboard"} />
-
-          <div className="row pr-4">
-            <div className="col-xl-12 col-sm-12 mb-3">
-              <div className="card text-white bg-primary o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Total Amount
-                    <br /> <b>Amount</b>
+        {loading ? <Loader /> : (
+            <>
+            <MetaData title={"Admin Dashboard"} />
+  
+            <div className="row pr-4">
+              <div className="col-xl-12 col-sm-12 mb-3">
+                <div className="card text-white bg-primary o-hidden h-100">
+                  <div className="card-body">
+                    <div className="text-center card-font-size">
+                      Total Amount
+                      <br />  <b>${totalAmount && totalAmount.toFixed(2)} </b>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="row pr-4">
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-success o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Products
-                    <br /> <b> Products</b>
+  
+            <div className="row pr-4">
+              <div className="col-xl-3 col-sm-6 mb-3">
+                <div className="card text-white bg-success o-hidden h-100">
+                  <div className="card-body">
+                    <div className="text-center card-font-size">
+                      Products
+                      <br /> <b>{products && products.length}</b>
+                    </div>
                   </div>
-                </div>
-                <Link
-                  className="card-footer text-white clearfix small z-1"
-                  to="/admin/products"
-                >
-                  <span className="float-left">View Details</span>
-                  <span className="float-right">
-                    <i className="fa fa-angle-right"></i>
-                  </span>
-                </Link>
-              </div>
-            </div>
-
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-danger o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Orders
-                    <br /> <b>Orders</b>
-                  </div>
-                </div>
-                <Link
-                  className="card-footer text-white clearfix small z-1"
-                  to="/admin/orders"
-                >
-                  <span className="float-left">View Details</span>
-                  <span className="float-right">
-                    <i className="fa fa-angle-right"></i>
-                  </span>
-                </Link>
-              </div>
-            </div>
-
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-info o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Users
-                    <br /> <b>Users</b>
-                  </div>
-                </div>
-                <Link
-                  className="card-footer text-white clearfix small z-1"
-                  to="/admin/users"
-                >
-                  <span className="float-left">View Details</span>
-                  <span className="float-right">
-                    <i className="fa fa-angle-right"></i>
-                  </span>
-                </Link>
-              </div>
-            </div>
-
-            <div className="col-xl-3 col-sm-6 mb-3">
-              <div className="card text-white bg-warning o-hidden h-100">
-                <div className="card-body">
-                  <div className="text-center card-font-size">
-                    Out of Stock
-                    <br /> <b>Best done</b>
-                  </div>
+                  <Link
+                    className="card-footer text-white clearfix small z-1"
+                    to="/admin/products"
+                  >
+                    <span className="float-left">View Details</span>
+                    <span className="float-right">
+                      <i className="fa fa-angle-right"></i>
+                    </span>
+                  </Link>
                 </div>
               </div>
+  
+              <div className="col-xl-3 col-sm-6 mb-3">
+                <div className="card text-white bg-danger o-hidden h-100">
+                  <div className="card-body">
+                    <div className="text-center card-font-size">
+                      Orders
+                      <br /> <b>{orders && orders.length}</b>
+                    </div>
+                  </div>
+                  <Link
+                    className="card-footer text-white clearfix small z-1"
+                    to="/admin/orders"
+                  >
+                    <span className="float-left">View Details</span>
+                    <span className="float-right">
+                      <i className="fa fa-angle-right"></i>
+                    </span>
+                  </Link>
+                </div>
+              </div>
+  
+              <div className="col-xl-3 col-sm-6 mb-3">
+                <div className="card text-white bg-info o-hidden h-100">
+                  <div className="card-body">
+                    <div className="text-center card-font-size">
+                      Users
+                      <br /> <b>{users && users.length}</b>
+                    </div>
+                  </div>
+                  <Link
+                    className="card-footer text-white clearfix small z-1"
+                    to="/admin/users"
+                  >
+                    <span className="float-left">View Details</span>
+                    <span className="float-right">
+                      <i className="fa fa-angle-right"></i>
+                    </span>
+                  </Link>
+                </div>
+              </div>
+  
+              <div className="col-xl-3 col-sm-6 mb-3">
+                <div className="card text-white bg-warning o-hidden h-100">
+                  <div className="card-body">
+                    <div className="text-center card-font-size">
+                      Out of Stock
+                      <br />  <b>{outOfStock}</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </>
+          </>
+          
+ )}
+      
       </div>
     </div>
   );
