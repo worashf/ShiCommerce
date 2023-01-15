2d9fbfd72923b30e5b5b30701f9aa1b730d552b8
Worash <uppert83@gmail.com>
2023-01-15 15:37:31 +0300

    Implement admin order listing and deletion

---
+++ frontend/src/components/admin/OrderList.js
@@ -0,0 +1,126 @@
+import React, { Fragment, useEffect } from 'react'
+import { Link, useNavigate} from 'react-router-dom'
+import { MDBDataTable } from 'mdbreact'
+
+import MetaData from '../layout/MetaData'
+import Loader from '../layout/Loader'
+import Sidebar from './Sidebar'
+
+import { toast } from 'react-toastify'
+import { useDispatch, useSelector } from 'react-redux'
+import { allOrders, deleteOrder, clearErrors } from '../../redux/actions/orderActions'
+import { DELETE_ORDER_RESET } from '../../redux/constants/orderConstants'
+
+const OrdersList = ({ history }) => {
+
+
+    const dispatch = useDispatch();
+    const navigate  = useNavigate()
+    const { loading, error, orders } = useSelector(state => state.allOrders);
+    const { isDeleted } = useSelector(state => state.orderAction)
+
+    useEffect(() => {
+        dispatch(allOrders());
+
+        if (error) {
+            toast.error(error);
+            dispatch(clearErrors())
+        }
+
+        if (isDeleted) {
+            toast.success('Order deleted successfully');
+            navigate('/admin/orders');
+            dispatch({ type: DELETE_ORDER_RESET })
+        }
+
+    }, [dispatch, toast, error, isDeleted, navigate])
+
+    const deleteOrderHandler = (id) => {
+        dispatch(deleteOrder(id))
+    }
+
+    const setOrders = () => {
+        const data = {
+            columns: [
+                {
+                    label: 'Order ID',
+                    field: 'id',
+                    sort: 'asc'
+                },
+                {
+                    label: 'No of Items',
+                    field: 'numofItems',
+                    sort: 'asc'
+                },
+                {
+                    label: 'Amount',
+                    field: 'amount',
+                    sort: 'asc'
+                },
+                {
+                    label: 'Status',
+                    field: 'status',
+                    sort: 'asc'
+                },
+                {
+                    label: 'Actions',
+                    field: 'actions',
+                },
+            ],
+            rows: []
+        }
+
+        orders.forEach(order => {
+            data.rows.push({
+                id: order._id,
+                numofItems: order.orderItems.length,
+                amount: `$${order.totalPrice}`,
+                status: order.orderStatus && String(order.orderStatus).includes('Delivered')
+                    ? <p style={{ color: 'green' }}>{order.orderStatus}</p>
+                    : <p style={{ color: 'red' }}>{order.orderStatus}</p>,
+                actions: <>
+                    <Link to={`/admin/order/${order._id}`} className="btn btn-primary py-1 px-2">
+                        <i className="fa fa-eye"></i>
+                    </Link>
+                    <button className="btn btn-danger py-1 px-2 ml-2" onClick={() => deleteOrderHandler(order._id)}>
+                        <i className="fa fa-trash"></i>
+                    </button>
+                </>
+            })
+        })
+
+        return data;
+    }
+
+
+    return (
+        <>
+            <MetaData title={'All Orders'} />
+            <div className="row">
+                <div className="col-12 col-md-2">
+                    <Sidebar />
+                </div>
+
+                <div className="col-12 col-md-10">
+                    <>
+                        <h1 className="my-5">All Orders</h1>
+
+                        {loading ? <Loader /> : (
+                            <MDBDataTable
+                                data={setOrders()}
+                                className="px-3"
+                                bordered
+                                striped
+                                hover
+                            />
+                        )}
+
+                    </>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default OrdersList
