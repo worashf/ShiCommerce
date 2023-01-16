f0c9cd72fc790e0700a1d841e26d28f1e91e424f
Worash <uppert83@gmail.com>
2023-01-16 11:11:22 +0300

    Implement update, delete user

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -9,7 +9,16 @@ import {
     UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAIL, FORGOT_PASSWORD_REQUEST,
     FORGOT_PASSWORD_SUCCESS, FORGOT_PASSWORD_FAIL,
     NEW_PASSWORD_REQUEST, NEW_PASSWORD_SUCCESS, NEW_PASSWORD_FAIL,
-    ALL_USERS_REQUEST,ALL_USERS_SUCCESS,ALL_USERS_FAIL,
+    ALL_USERS_REQUEST, ALL_USERS_SUCCESS, ALL_USERS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    UPDATE_USER_REQUEST,
+    UPDATE_USER_SUCCESS,
+    UPDATE_USER_FAIL,
+    DELETE_USER_REQUEST,
+    DELETE_USER_SUCCESS,
+    DELETE_USER_FAIL,
     CLEAR_ERRORS
 } from "../constants/userConstants"
  
@@ -234,7 +243,77 @@ export const allUsers = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ALL_USERS_FAIL,
-            payload: error.response.data.message
+            payload: error.response.data.errorMessage
+        })
+    }
+}
+
+// Delete user - ADMIN
+export const deleteUser = (id) => async (dispatch) => {
+    try {
+
+        dispatch({ type: DELETE_USER_REQUEST })
+
+        const { data } = await axios.delete(`/api/v1/admin/user/${id}`)
+
+        dispatch({
+            type: DELETE_USER_SUCCESS,
+            payload: data.success
+        })
+
+    } catch (error) {
+        dispatch({
+            type: DELETE_USER_FAIL,
+            payload: error.response.data.errorMessage
+        })
+    }
+}
+
+// Get user details - ADMIN
+export const getUserDetails = (id) => async (dispatch) => {
+    try {
+
+        dispatch({ type: USER_DETAILS_REQUEST })
+
+
+        const { data } = await axios.get(`/api/v1/admin/user/${id}`)
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data.user
+        })
+
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: error.response.data.errorMessage
+        })
+    }
+}
+
+// Update user - ADMIN
+export const updateUser = (id, userData) => async (dispatch) => {
+    try {
+
+        dispatch({ type: UPDATE_USER_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.put(`/api/v1/admin/user/${id}`, userData, config)
+
+        dispatch({
+            type: UPDATE_USER_SUCCESS,
+            payload: data.success
+        })
+
+    } catch (error) {
+        dispatch({
+            type: UPDATE_USER_FAIL,
+            payload: error.response.data.errorMessage
         })
     }
 }
