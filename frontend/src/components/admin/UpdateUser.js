2e6b8de625f3e53414d3a2e56ac15b9095d9e04c
Worash <uppert83@gmail.com>
2023-01-16 11:02:32 +0300

    Implement  admin  update user information

---
+++ frontend/src/components/admin/UpdateUser.js
@@ -0,0 +1,129 @@
+import React, {  useState, useEffect } from 'react'
+
+import MetaData from '../layout/MetaData'
+import Sidebar from './Sidebar'
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast} from 'react-toastify'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUser, getUserDetails, clearErrors } from '../../redux/actions/userAction'
+import { UPDATE_USER_RESET } from '../../redux/constants/userConstants'
+
+const UpdateUser = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [role, setRole] = useState('')
+
+   
+    const dispatch = useDispatch();
+ const navigate = useNavigate()
+    const { error, isUpdated } = useSelector(state => state.user);
+    const { user } = useSelector(state => state.userDetails)
+
+    const { userId } = useParams();
+
+    useEffect(() => {
+
+        console.log(user && user._id !== userId);
+        if (user && user._id !== userId) {
+            dispatch(getUserDetails(userId))
+        } else {
+            setName(user.name);
+            setEmail(user.email);
+            setRole(user.role)
+        }
+
+        if (error) {
+           toast.error(error);
+            dispatch(clearErrors());
+        }
+
+        if (isUpdated) {
+           toast.success('User updated successfully')
+
+        navigate('/admin/users')
+
+            dispatch({
+                type: UPDATE_USER_RESET
+            })
+        }
+
+    }, [dispatch,toast, error, navigate, isUpdated, userId, user])
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.set('name', name);
+        formData.set('email', email);
+        formData.set('role', role);
+
+        dispatch(updateUser(user._id, formData))
+    }
+
+
+    return (
+        <>
+            <MetaData title={`Update User`} />
+            <div className="row">
+                <div className="col-12 col-md-2">
+                    <Sidebar />
+                </div>
+
+                <div className="col-12 col-md-10">
+                    <div className="row wrapper">
+                        <div className="col-10 col-lg-5">
+                            <form className="shadow-lg" onSubmit={submitHandler}>
+                                <h1 className="mt-2 mb-5">Update User</h1>
+
+                                <div className="form-group">
+                                    <label htmlFor="name_field">Name</label>
+                                    <input
+                                        type="name"
+                                        id="name_field"
+                                        className="form-control"
+                                        name='name'
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="email_field">Email</label>
+                                    <input
+                                        type="email"
+                                        id="email_field"
+                                        className="form-control"
+                                        name='email'
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="role_field">Role</label>
+
+                                    <select
+                                        id="role_field"
+                                        className="form-control"
+                                        name='role'
+                                        value={role}
+                                        onChange={(e) => setRole(e.target.value)}
+                                    >
+                                        <option value="user">user</option>
+                                        <option value="admin">admin</option>
+                                    </select>
+                                </div>
+
+                                <button type="submit" className="btn update-btn btn-block mt-4 mb-3" >Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default UpdateUser
